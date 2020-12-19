86bac5d2b36311d05592dd77ca9c485b36c6b97d
scdt <torfinkarlsefni@gmail.com>
2020-12-18 21:09:39 -0500

    scores

--- htmlClassifier.py
+++ htmlClassifier.py
@@ -6,8 +6,10 @@ import html2text as h2t
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.metrics import accuracy_score
-from catboost import CatBoostClassifier, Pool
+from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from catboost import CatBoostClassifier
 
 print(catboost.__file__)
 
@@ -68,16 +70,29 @@ def trainAndTest():
     for html in test_htmls:
         test_texts.append(htmlToText(html))
 
-    vectorizer = CountVectorizer()
-    train_set = vectorizer.fit_transform(train_texts).toarray()
-    test_set = vectorizer.fit_transform(test_texts).toarray()
-    clf = CatBoostClassifier(iterations=1000, learning_rate=3, depth=7)
-
-    clf.fit(X=train_set, y=train_classes)
-    predict_train = clf.predict(train_set)
-    predict = clf.predict(test_set)
-    print('accuracy_train:', accuracy_score(train_classes, predict_train))
-    print('accuracy_test:', accuracy_score(test_classes, predict))
+    vectorizer = TfidfVectorizer(max_features=200)
+    train_set = vectorizer.fit_transform(train_texts)
+    test_set = vectorizer.transform(test_texts)
+    clfMNB = MultinomialNB()
+    clfLSVC = LinearSVC()
+    clfCB = CatBoostClassifier(iterations=100, learning_rate=3, depth=7)
+    clfMNB.fit(X=train_set, y=train_classes)
+    clfLSVC.fit(X=train_set, y=train_classes)
+    clfCB.fit(X=train_set.toarray(), y=train_classes)
+    predict_trainMNB = clfMNB.predict(train_set)
+    predict_trainLSVC = clfLSVC.predict(train_set)
+    predictMNB = clfMNB.predict(test_set)
+    predictLSVC = clfLSVC.predict(test_set)
+    predictCB = clfCB.predict(test_set.toarray())
+    print('f1_scoreMNB:', f1_score(test_classes, predictMNB))
+    print('precision_scoreMNB:', precision_score(test_classes, predictMNB))
+    print('recall_scoreMNB:', recall_score(test_classes, predictMNB))
+    print('f1_scoreLSVC:', f1_score(test_classes, predictLSVC))
+    print('precision_scoreLSVC:', precision_score(test_classes, predictLSVC))
+    print('recall_scoreLSVC:', recall_score(test_classes, predictLSVC))
+    print('f1_scoreCB:', f1_score(test_classes, predictCB))
+    print('precision_scoreCB:', precision_score(test_classes, predictCB))
+    print('recall_scoreCB:', recall_score(test_classes, predictCB))
 
 
 if __name__ == '__main__':
